acc41be05f588caeccc6f89614a219b987db46be
cosminmarinica153 <marinica.cosmin2002@gmail.com>
2024-06-21 20:25:19 +0300

    Added sum of two numbers

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("The sum is: " + sum);
     }
 }
